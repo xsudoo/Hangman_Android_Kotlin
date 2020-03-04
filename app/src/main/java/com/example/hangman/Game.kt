228c89ee5e1de81fa892b3cb47a8f308e57d6f38
Xsudo <dkiedrowicz@o2.pl>
2020-03-04 20:26:14 +0100

    commit

--- app/src/main/java/com/example/hangman/Game.kt
+++ app/src/main/java/com/example/hangman/Game.kt
@@ -2,49 +2,78 @@ package com.example.hangman
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 import kotlinx.android.synthetic.main.content_game.*
 import kotlin.random.Random
 
 class Game : AppCompatActivity() {
+    private var gameScore = 0
+    private var missShot = 0
+    private val listaSlow: ArrayList<String> = arrayListOf("Pierwszy", "Drugi", "Trzeci")
+    private var charList: ArrayList<Char> = arrayListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        var charList : ArrayList<Char> = arrayListOf('P','D','T')
-        //wstepna baza slow
-        val listaSlow : ArrayList<String> = arrayListOf("Pierwszy", "Drugi", "Trzeci")
 
+        //wstepna baza slow
+
+        val slowo = setWord(listaSlow)
         //Ustawienie obrazku
         gameImage.setImageResource(R.drawable.volkswagen)
 
         //Ustawienie pierwszego slowa
-        gameTextView.text = ukrywanieSlowa(setWord(listaSlow),charList)
+        gameTextView.text = wyswietlanie(ukrywanieSlowa(slowo, charList))
 
         // Obsluga button click
-        gameButton.setOnClickListener { gameTextView.text = ukrywanieSlowa(setWord(listaSlow),charList)}
+        gameButton.setOnClickListener {
+            val singleChar: Char = gameEditText.text.toString()[0].toUpperCase()
+            charList.add(singleChar)
+
+            if (czyTrafil(singleChar, slowo)) {
+                gameTextView.text = wyswietlanie(ukrywanieSlowa(slowo, charList))
+                gameImage.setImageResource(R.drawable.volkswagen)
+            } else {gameImage.setImageResource(R.drawable.star)
+            missShot++}
+
+        }
     }
 
-    private fun randomNumber (lista : ArrayList<String>) :Int{
-        return Random.nextInt(0,lista.size)
+    private fun setWord(lista: ArrayList<String>): String {
+        return lista[Random.nextInt(0, lista.size)].toUpperCase()
     }
 
-    private fun setWord (lista: ArrayList<String>) :String{
-        return lista[Random.nextInt(0,lista.size)]
-    }
-
-    private fun ukrywanieSlowa (word:String, charList:ArrayList<Char> ):String{
+    private fun ukrywanieSlowa(word: String, charList: ArrayList<Char>): String {
         var second = ""
 
-        for (singleChar in word.toUpperCase()){
+        for (singleChar in word) {
             when {
-                charList.contains(singleChar) -> {second+=singleChar}
-                singleChar.isWhitespace() -> second+=" "
-                else -> second+=("_")
+                charList.contains(singleChar) -> {
+                    second += singleChar
+                }
+                singleChar.isWhitespace() -> second += " "
+                else -> second += ("_")
+
             }
+
         }
+        gameScore++
         return second
     }
+
+    private fun wyswietlanie(word: String): String {
+        var trzecie = ""
+
+        for (single in word) {
+            trzecie += "$single "
+        }
+
+        return trzecie
+    }
+
+    private fun czyTrafil(singleChar: Char, word: String): Boolean {
+
+        return word.contains(singleChar)    }
 
 }
